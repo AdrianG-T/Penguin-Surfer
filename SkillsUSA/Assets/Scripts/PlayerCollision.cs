3eaf3d26184001001dfc5b923947c2af85bec4cc
Adrian <adriangtrejo@gmail.com>
2019-07-15 15:33:36 -0500

    new file:   SkillsUSA/Library/metadata/fa/fa27413e9edb06a6058d6c894eca0338

---
+++ SkillsUSA/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour{
+
+    public PlayerMovement movement;
+    ///public Rigidbody rb;
+    //public float explosionforce = 100f ;
+    ///public float explosionradius = 100f;
+    ///public float upwardsmodifier = 100f;
+    ///public Vector3 explosionoffset;
+
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            ///rb.AddExplosionForce(explosionforce, rb.position, explosionradius, upwardsmodifier, ForceMode.VelocityChange);
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+
+}
---
+++ SkillsUSA/shutdownpi.py
@@ -0,0 +1,6 @@
+import RPi.GPIO as GPIO
+import time
+import breadboard
+
+GPIO.setwarnings(False)
+GPIO.setmode()
